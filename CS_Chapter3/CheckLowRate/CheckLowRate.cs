78140f2a3f5ef8d9c22bfd68354dc37993a30948
roscioli99 <ryanroscioli99@gmail.com>
2017-02-07 14:13:19 -0500

    Completed CheckLowRate

---
+++ CS_Chapter3/CheckLowRate/CheckLowRate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CheckLowRate
+{
+    class CheckLowRate
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your hourly pay rate?");
+            String payRate = Console.ReadLine();
+            double pay = Convert.ToDouble(payRate);
+
+            if (pay < 5.65)
+                Console.WriteLine("ERROR: PAY RATE TOO LOW");
+            else
+                Console.WriteLine("Your hourly payrate is " + pay);
+        }
+    }
+}
